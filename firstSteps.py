c91e0174c1f69a5004f27978ccf490182277b55c
cannotilever <51347140+cannotilever@users.noreply.github.com>
2022-06-29 18:09:30 -0400

    Update firstSteps.py

--- firstSteps.py
+++ firstSteps.py
@@ -3,11 +3,10 @@ import time
 #You can define helper functions here, make sure to but them *above* the main function
 startTime = time.time()
 def main(robot: libhousy.robot):
-    global startTime
     #Here is where your recurring code will go
     if time.time() - startTime < 2:
-        robot.lDrive.Set(0.5)
-        robot.rDrive.Set(0.5)
+        robot.lDrive.Set(0.4)
+        robot.rDrive.Set(0.4)
     else:
         robot.lDrive.Set(0)
         robot.rDrive.Set(0)
